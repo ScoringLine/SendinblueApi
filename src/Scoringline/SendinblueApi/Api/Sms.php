f1204346778f9e75f212821b6d2c0c74271a89e4
Nek (Maxime Veber) <nek.dev@gmail.com>
2015-04-28 16:20:51 +0200

    Added copyright on files

--- src/Scoringline/SendinblueApi/Api/Sms.php
+++ src/Scoringline/SendinblueApi/Api/Sms.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is a part of scoringline sendinblue api package
+ *
+ * (c) Scoringline <m.veber@scoringline.com>
+ *
+ * For the full license, take a look to the LICENSE file
+ * on the root directory of this project
+ */
+
 namespace Scoringline\SendinblueApi\Api;
 
 use Nekland\BaseApi\Api\AbstractApi;
